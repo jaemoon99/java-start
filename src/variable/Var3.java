99918467ccc1bdb4120771fa5651c5aadd44d11c
jaemoon <woans5970@naver.com>
2023-11-28 20:03:41 +0900

    section 2: 변수 값 변경

---
+++ src/variable/Var3.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class Var3 {
+
+    public static void main(String[] args) {
+        int a; // 변수 선언
+        a = 10; // 변수를 10으로 초기화
+        System.out.println(a);
+        a = 50; // 재할당
+        System.out.println(a);
+
+    }
+}
