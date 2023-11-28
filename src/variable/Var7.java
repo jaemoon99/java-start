410d670b2c6462da559a88bf051611290030f60c
jaemoon <woans5970@naver.com>
2023-11-28 20:27:14 +0900

    section 2: 변수 타입 1

---
+++ src/variable/Var7.java
@@ -0,0 +1,20 @@
+package variable;
+
+public class Var7 {
+
+    public static void main(String[] args) {
+        int a = 100; // 정수
+        double b = 10.5; // 실수
+        boolean c = true; // 불리언(boolean) true, false 만 입력 가능
+        char d = 'A'; // 문자 하나
+        String e = "Hello Java"; //문자열, 문자열을 다루기 위한 특별한 타입
+
+//        int z = "100"; // 타입과 다른 리터럴을 입력할 경우 오류 발생
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
