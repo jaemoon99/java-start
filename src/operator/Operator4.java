57476ae423269748ac05c2acf4f5b8986a7259bb
jaemoon <woans5970@naver.com>
2023-11-29 10:52:32 +0900

    section 3: 연산자 우선순위

---
+++ src/operator/Operator4.java
@@ -0,0 +1,11 @@
+package operator;
+
+public class Operator4 {
+
+    public static void main(String[] args) {
+        int sum3 = 2 * 2 + 3 * 3;
+        int sum4 = (2 * 2) + (3 * 3); // 식이 길면 식별하기 편하게 괄호를 넣어준다.
+        System.out.println("sum3 = " + sum3);
+        System.out.println("sum4 = " + sum4);
+    }
+}
